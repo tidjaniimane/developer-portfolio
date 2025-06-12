1e27b753285fd9c2c2422b66e537550a1a414303
tidjaniimane <imanetidjani1174@gmail.com>
2025-06-12 07:17:29 -1100

    Add start script for Render

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
 });
+app.get('/', (req, res) => {
+  res.send('Hello from the Developer Portfolio backend!');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
