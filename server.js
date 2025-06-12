693d092085399ed4e6fc25e3b3356f48d85cefc5
tidjaniimane <imanetidjani1174@gmail.com>
2025-06-12 08:11:23 -1100

    Fix: added working server.js and updated package.json

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
+// server.js
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
-app.get('/', (req, res) => {
-  res.send('Hello from the Developer Portfolio backend!');
-});
+app.use(express.static('public')); // Serve static files from "public" folder
 
+app.get('/', (req, res) => {
+  res.send('Hello from Developer Portfolio!');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
